e94fb3a19ea92dc5463f43c8fd04f6c8db748c9b
Amaury941 <69680986+Amaury941@users.noreply.github.com>
2021-08-27 10:00:54 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const {ApolloServer, gql} =  require('apollo-server');
 
+// "models"
+
 const typeDefs = gql`
     type Query {
         genesysOne: String
         getFathers:[Father!]!
-        getFatherById(id: String!):Father!
+        getFatherById(id: String!):Father
     }
 
     type Father {
@@ -32,26 +34,34 @@ const typeDefs = gql`
             name: String!,
             age: Int!,
         ): Son!,
-    
-    }
 
+        removeFather(
+            FatherId: String!
+        ):Father, 
+    }
 `;
+
+// nosso banco de dados
 
 const fathers = [
     {_id:"0", name:"adam",age:99, son: {name:"Abel", age:22},skipSon:true},
     {_id:"1", name:"noah",age:35, son: {name:"Michael", age:19}}
 ]
 
+// resolvers
+
 const resolvers = {
     Query: {
         genesysOne: () => "In the beginning, God created the heavens and the earth.", //essa query retorna a mensagem
         getFathers: () => fathers,
+        // argumentos: id do pai
         getFatherById: (_,args) => {
             return fathers.find((user)=> user._id === args.id)
         },
     },
 
     Mutation: {
+        // argumentos: dados do pai
         addFather: (_,args) => {
             const newFather = {
                 _id: String(fathers.length),
@@ -61,7 +71,13 @@ const resolvers = {
             fathers.push(newFather);
             return newFather;
         },
-
+        // argumentos: id do pai
+        removeFather: (_,args) => {
+            const father = fathers.find((user)=> user._id === args.FatherId)
+            fathers.splice(parseInt(args.FatherId),1);
+            return father
+        },
+        // argumentos: id do pai
         addSon: (_,args) => {
             const newSon = {
                 _id  : String(args.FatherId + "'" +'s son'),
@@ -78,11 +94,11 @@ const resolvers = {
 
             fathers.splice(parseInt(args.FatherId),1,newFather)
             return newSon;
-        } 
-
+        },
     },
-
 };
 
+// Ligando o servidor
+
 const server = new ApolloServer({typeDefs,resolvers});
-server.listen().then(({url}) => console.log(`server started at ${url}`));
+server.listen().then(({url}) => console.log(`server iniciado na url ${url}`));
